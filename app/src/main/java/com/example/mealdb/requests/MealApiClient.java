7481f8fdf4cc47cf3ebb66e45704dd79e66d7a16
mkix <han109k@gmail.com>
2020-02-12 22:35:35 +0300

    MealApiClient implemented

---
+++ app/src/main/java/com/example/mealdb/requests/MealApiClient.java
@@ -0,0 +1,174 @@
+package com.example.mealdb.requests;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.mealdb.AppExecutors;
+import com.example.mealdb.model.Meal;
+import com.example.mealdb.requests.responses.MealResponse;
+import com.example.mealdb.requests.responses.MealSearchResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+import static com.example.mealdb.utils.Constants.NETWORK_TIMEOUT;
+
+public class MealApiClient {
+
+    private static final String TAG = "MealApiClient";
+
+    private static MealApiClient instance;
+    private MutableLiveData<List<Meal>> mMeals;
+    private MutableLiveData<Meal> mMeal;
+    private RetrieveMealsRunnable mRetrieveMealsRunnable;
+    private RetrieveMealRunnable mRetrieveMealRunnable;
+    private MutableLiveData<Boolean> mMealRequestTimeout = new MutableLiveData<>();
+
+    public static MealApiClient getInstance() {
+        if (instance == null) {
+            return new MealApiClient();
+        }
+        return instance;
+    }
+
+    private MealApiClient() {
+        mMeal = new MutableLiveData<>();
+        mMeals = new MutableLiveData<>();
+    }
+
+    public LiveData<List<Meal>> getMeals() {
+        return mMeals;
+    }
+
+    public LiveData<Meal> getMeal() {
+        return mMeal;
+    }
+
+    public LiveData<Boolean> getMealRequestTimedOut() {
+        return mMealRequestTimeout;
+    }
+
+    public void searchMealsApi(String query) {
+        if(mRetrieveMealsRunnable != null){
+            mRetrieveMealsRunnable = null;
+        }
+
+        mRetrieveMealsRunnable = new RetrieveMealsRunnable(query);
+        final Future handler = AppExecutors.getInstance().networkIO().submit(mRetrieveMealsRunnable);
+
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                mMealRequestTimeout.postValue(true);
+                handler.cancel(true);
+            }
+        }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
+    private class RetrieveMealsRunnable implements Runnable {
+
+        private String query;
+        private boolean cancelRequest;
+
+        public RetrieveMealsRunnable(String query) {
+            this.query = query;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Response response = getMeals(query).execute();
+
+                if(cancelRequest)
+                    return;
+
+                if(response.code() == 200) {    // html status code (200)
+                    List<Meal> list = new ArrayList<>(((MealSearchResponse)response.body()).getMeals());
+                    mMeals.postValue(list);
+                } else {
+                    String error = response.errorBody().toString();
+                    Log.e(TAG, "run: " + error );
+                    mMeals.postValue(null);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                mMeals.postValue(null);
+            }
+        }
+
+        // make call on search query through REST
+        private Call<MealSearchResponse> getMeals(String query) {
+            return ServiceGenerator.getMealApi().searchMeal(
+                    query
+            );
+        }
+
+        private void cancelRequest() {
+            Log.d(TAG, "cancelRequest: search request cancelled");
+            cancelRequest = true;
+        }
+    }
+
+    public void searchMealById(String mealId) {
+        if(mRetrieveMealRunnable != null) {
+            mRetrieveMealRunnable = null;
+        }
+
+        mRetrieveMealRunnable = new RetrieveMealRunnable(mealId);
+        final Future handler = AppExecutors.getInstance().networkIO().submit(mRetrieveMealRunnable);
+
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                mMealRequestTimeout.postValue(true);
+                handler.cancel(true);
+            }
+        }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
+    private class RetrieveMealRunnable implements Runnable {
+
+        private String mealId;
+        private boolean cancelRequest;
+
+        public RetrieveMealRunnable(String mealId) {
+            this.mealId = mealId;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        private Call<MealResponse> getMeal(String mealId) {
+            return ServiceGenerator.getMealApi().getMeal(
+                    mealId
+            );
+        }
+
+        private void cancelRequest() {
+            Log.d(TAG, "cancelRequest: look up request cancelled");
+            cancelRequest = true;
+        }
+    }
+
+    public void cancelRequest() {
+        if(mRetrieveMealsRunnable != null) {
+            mRetrieveMealsRunnable.cancelRequest();
+        }
+        if(mRetrieveMealRunnable != null) {
+            mRetrieveMealRunnable.cancelRequest();
+        }
+    }
+}
