d14a74ca9cdbcfd540e782ae0357dbe9e6ac8008
mkix <han109k@gmail.com>
2020-02-15 19:01:07 +0300

    added MealActivity

---
+++ app/src/main/java/com/example/mealdb/MealActivity.java
@@ -0,0 +1,13 @@
+package com.example.mealdb;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MealActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
